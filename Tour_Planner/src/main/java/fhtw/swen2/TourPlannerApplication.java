ed973caa747e8f6a6334968a8598871faba3ca41
lesjanikel <lsaislynn@gmail.com>
2026-02-18 11:58:09 +0100

    Initial commit

---
+++ Tour_Planner/src/main/java/fhtw/swen2/TourPlannerApplication.java
@@ -0,0 +1,13 @@
+package fhtw.swen2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourPlannerApplication.class, args);
+	}
+
+}
---
+++ Tour_Planner/src/test/java/fhtw/swen2/Swen2ApplicationTests.java
@@ -0,0 +1,13 @@
+package fhtw.swen2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swen2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
